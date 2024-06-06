06f8801f69c1903aeb100e4566c5b9b761306416
Preetheesri <rpreetheesri@gmail.com>
2024-06-06 19:00:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app= express()
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+mongoose.connect('mongodb://localhost:27017/MoneyList')
+var db = mongoose.connection
+db.on('error', ()=> console.log("Error in connecting to the Database"))
+db.once('open', () => console.log("Connected to Database"))
+
+app.post("/add", (req,res) =>{
+    var category_select = req.body.category_select
+    var amount_input= req.body.amount_input
+    var info = req.body.info
+    var date_input = req.body.date_input
+
+    var data={
+        "Category": category_select,
+        "Amount" : amount_input,
+        "Info": info,
+        "Date": date_input
+    }
+    db.collection('users').insertOne(data, (err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully")
+
+    })
+})
+app.get("/",(req,res) =>{
+    res.set({
+        "Allow-access-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+});
+const PORT = process.env.PORT || 5001; // Change 5000 to 5001
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated');
+    });
+});
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('Process interrupted');
+    });
+});
